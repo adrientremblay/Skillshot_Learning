c6b22931152ca6a8685ffe5b9992d51be2ef7b9b
s-maurice <mauricemscholtes@gmail.com>
2020-01-07 17:25:22 +0100

    added player_ids, changed prepared model_act to act on specified player, in model_train prepared epoch saving lists, also prepared feature and targets lists for model_fit call, added prep for prepare_targets, and replay_training

--- SkillshotLearner.py
+++ SkillshotLearner.py
@@ -8,6 +8,7 @@ class SkillshotLearner(object):
         self.actions = None
         self.model = None
         self.game = SkillshotGame()
+        self.player_ids = (1, 2)
 
     def model_define(self):
         # defines and creates a model
@@ -21,7 +22,7 @@ class SkillshotLearner(object):
         # saves a model to the save location
         pass
 
-    def model_act(self, features, mutate_threshold):
+    def model_act(self, player_id, features, mutate_threshold):
         # checks threshold to see if model acts or random acts,
         # then takes features, feeds through model to find actions and performs them on model
         pass
@@ -31,34 +32,60 @@ class SkillshotLearner(object):
         for epoch in range(epochs):
             # reset the game
             self.game.game_reset()
+            # reset the epoch data lists
+            epoch_game_state = []
+
             # enter game
             while self.game.game_live:
                 # get the game state
                 game_state = self.game.get_state()
+
                 # do actions
-                self.model_act()
-                # get the reward
-                self.calculate_reward(game_state)
-                # save the game state and reward
+                for player_id in self.player_ids:
+                    self.model_act(player_id, self.prepare_features(game_state, player_id)[0], mutate_threshold)
+
+                # save the game state  - possibly also save the get_board here to visualise model later
+                epoch_game_state.append(game_state)
 
                 # tick the game
                 self.game.game_tick()
 
-            # after each epoch ends, fit the model
-            self.model_fit()
-        # after all epochs are completed
+            # after each epoch ends, print epoch performance
+            print("Epoch Completed")
+            # prepare to fit by extracting rewards from epoch_game_state
+            epoch_player_rewards = dict((player, []) for player in self.player_ids)
+            for game_state in epoch_game_state:
+                for player_id, opponent_id in zip(self.player_ids, self.player_ids[::-1]):
+                    epoch_player_rewards.get(player_id).append(self.calculate_reward(game_state, player_id, opponent_id))
+
+            # fit model
+            player_features, player_targets = [], []
+            for player_id in self.player_ids:
+                player_features + self.prepare_features(epoch_game_state, player_id)  # merge lists
+                player_targets + self.prepare_targets(epoch_player_rewards.get(player_id), player_id)  # merge lists
+            self.model_fit(player_features, player_targets)  # fit can be called a single time
+
+        # after all epochs are completed, print overall performance
         print("Training Completed")
         # save model
         self.model_save()
 
     def model_fit(self, features, targets):
         # after each game is played, fit the model
+        # shuffle features, targets and train with lower batch size for increased generalisation
         pass
 
-    def prepare_inputs(self, features, player_id):
+    @staticmethod
+    def prepare_features(features, player_id):
         # prepares the model inputs / reshapes for model
-        # for model trainijng against self, the dict will need to be flipped to keep consistent "self" player
-        pass
+        # for model training against self, the dict will need to be flipped to keep consistent "self" player
+        return [0]  # returns list
+
+    @staticmethod
+    def prepare_targets(targets, player_id):
+        # prepares the model targets / reshapes for model
+        # for model training against self, the dict will need to be flipped to keep consistent "self" player
+        return [0]  # returns list
 
     @staticmethod
     def calculate_reward(features, player_id, opponent_id, on_target_multiplier_change=0.5):
@@ -85,4 +112,8 @@ class SkillshotLearner(object):
 
     def plot_training(self):
         # plots the training progress
+        pass
+
+    def replay_training(self, boards):
+        # takes list of boards and displays them using pygame to visualise training afterwards
         pass
